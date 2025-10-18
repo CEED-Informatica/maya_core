5063d1468d4ca61f001b1a28ad58b70bd9573aa0
Alfredo Oltra <aoltra@gmail.com>
2025-10-18 09:05:34 +0000

    feat: se añade nueva opción de configuración: moodle_user_admin

--- models/config_settings.py
+++ models/config_settings.py
@@ -7,3 +7,6 @@ class ResConfigSettings(models.TransientModel):
     moodle_user = fields.Char(string = 'Usuario acceso a Moodle', config_parameter='maya_core.moodle_user',
                               help = 'Usuario (clave) definido en .maya_moodleteacher')
     moodle_url = fields.Char(string = 'URL del servidor de Moodle', config_parameter='maya_core.moodle_url')
+
+    moodle_user_admin = fields.Char(string = 'Administrador acceso a Moodle', config_parameter='maya_core.moodle_user_admin',
+                              help = 'Usuario (clave) definido en .maya_moodleteacher con permisos de administración en Moodle')
