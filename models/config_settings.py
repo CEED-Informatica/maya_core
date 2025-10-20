a7b054d315ca53e9bd7feb9754cb210515cfe9bb
Alfredo Oltra <aoltra@gmail.com>
2025-10-20 08:01:34 +0000

    feat: nuevas opciones de configuración: alias_mail_maya, alias_mail_center, itaca_users_data

--- models/config_settings.py
+++ models/config_settings.py
@@ -10,3 +10,13 @@ class ResConfigSettings(models.TransientModel):
 
     moodle_user_admin = fields.Char(string = 'Administrador acceso a Moodle', config_parameter='maya_core.moodle_user_admin',
                               help = 'Usuario (clave) definido en .maya_moodleteacher con permisos de administración en Moodle')
+    
+    alias_mail_maya = fields.Char(string = 'Alias servidor de correo Maya', config_parameter='maya_core.alias_maya_mail',
+                              help = 'Alias en la configuración de Odoo del servidor de correo que envia correo con el nombre de Maya')
+    
+    alias_mail_center = fields.Char(string = 'Alias servidor de correo Centro', config_parameter='maya_core.alias_center_mail',
+                              help = 'Alias en la configuración de Odoo del servidor de correo que envia correo con el nombre del centro')
+
+    itaca_users_data = fields.Char(string = 'Fichero de datos de alumnado de Itaca', config_parameter='maya_core.itaca_students_data',
+                              help = 'Nombre del fichero .csv (debe incluir la extensión) que contiene los datos de los alumnos de Itaca. El fichero se almacena en /mnt/odoo-repo/itaca. Se genera con el script upload_itaca_students')
+
