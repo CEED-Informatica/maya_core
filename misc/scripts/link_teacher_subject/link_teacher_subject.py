5d5fffad3aa6c51729523863f6d5550cbc0b2dcf
Alfredo Oltra <aoltra@gmail.com>
2025-10-20 21:31:51 +0000

    feat: se simplifica el formato y la lectura del fichero csv

--- misc/scripts/link_teacher_subject/link_teacher_subject.py
+++ misc/scripts/link_teacher_subject/link_teacher_subject.py
@@ -19,7 +19,7 @@ def print_list(list):
   for value in list:  
     print(' * {}'.format(value))
 
-print('\033[1mMaya | link-subject-techer. v1.0\033[0m')
+print('\033[1mMaya | link-subject-techer. v2.0\033[0m')
 
 parser = argparse.ArgumentParser(
   description = 'Enlaza profesores (employee) y con módulos (subject) desde un csv')
@@ -39,7 +39,8 @@ db = args.database
 username = args.user
 password = args.password
 
-teacher_subjects = {}
+teachers_subjects_rel = []
+teachers = set()
 
 # end point xmlrpc/2/common permite llamadas sin autenticar
 print('\033[0;32m[INFO]\033[0m Conectando con',url, ' -> ', db)
@@ -61,32 +62,30 @@ try:
     line_count = 0
     for row in csv_reader:
       if line_count > 0:
-        # almaceno los módulos
-        subjects_row = row[1:]
-        if len(subjects_row) > 0 and len(subjects_row) % 2 == 0: # tienen que ser parejas
-          teacher_subjects[row[0]] = [(subjects_row[i], 
-                                       subjects_row[i + 1][:subjects_row[i + 1].find('-')].strip(), 
-                                       subjects_row[i + 1][(subjects_row[i + 1].find('-')+1):].strip()) 
-                                       for i in range(0, len(subjects_row), 2)]
-        else:
-          raise Exception(f'El usuario {row[0]} no tiene módulos asignados o el número de ciclos no coincide con el de módulos')
-
+        teachers_subjects_rel.append({
+          'teacher': row[0],
+          'course': row[1],
+          'subject': row[2],
+        })
+        teachers.add(row[0])
       line_count += 1
+
+
 except Exception as e:
   print('\033[0;31m[ERROR]\033[0m ' + str(e))
   print(f'\033[0;32m[INFO]\033[0m Saliendo...')
   exit()     
 
-print('\033[0;32m[INFO]\033[0m Usuarios en csv:', len(teacher_subjects))    
+print('\033[0;32m[INFO]\033[0m Módulos asignados a profesores en csv:', len(teachers_subjects_rel))    
 
 models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(url))
 
 try:
   # ciclos
-  course_output = models.execute_kw(db, uid, password, 'maya_core.course', 'search_read', [[]], { 'fields':  ['abbr', 'id'] } )
+  course_output = models.execute_kw(db, uid, password, 'maya_core.course', 'search_read', [[]], { 'fields':  ['abbr', 'id', 'code'] } )
   courses_ec = {}
   for course in course_output:
-    courses_ec[course['abbr']] = course['id']
+    courses_ec[course['code']] = [course['id'], course['abbr']]
 
   print(f'\033[0;32m[INFO]\033[0m Ciclos:')
   print_dictionary(courses_ec)
@@ -95,7 +94,7 @@ try:
   subject_output = models.execute_kw(db, uid, password, 'maya_core.subject', 'search_read', [[]], { 'fields':  ['abbr', 'code', 'id', 'courses_ids']})
   subjects_ec = {}
   for subj in subject_output:
-    subjects_ec[subj['code']] = [subj['id'], subj['courses_ids'] ]
+    subjects_ec[subj['code']] = [subj['id'], subj['courses_ids'], subj['abbr'] ]
 
   print(f'\033[0;32m[INFO]\033[0m Módulos:')
   print_dictionary(subjects_ec)
@@ -108,45 +107,60 @@ except Exception as e:
 line_count_OK = 0
 line_count_ERROR = 0
 
-for teacher in teacher_subjects:  
-  print("\033[0;32m[INFO]\033[0m Procesando", teacher)
+for teacher in teachers:
+  # hay que buscar si el empleado existe.
+  # obtengo el id del usuario
+  id = models.execute_kw(db, uid, password, 'res.users', 'search_read', 
+                                          [[['login','=', teacher]]], { 'fields': ['id', 'maya_employee_id', 'name']})
+  
+  if len(id) == 0:
+    raise Exception(f'No existe el usuario {teacher} en Odoo.')
+  
+  if id[0]['maya_employee_id'] == False:
+    raise Exception(f'No existe un empleado asociado al usuario {tsr["teacher"]}.')
+  
+  # se eliminan, si las hay, las relaciones previas con otros módulos
+  # es decir, cada vez que ejecuto el script se actualizan todas las relaciones
+  teacher_of_subjects = models.execute_kw(db, uid, password, 
+                                              'maya_core.subject_employee_rel', 
+                                              'search_read', 
+                                              [[['employee_id','=', id[0]['maya_employee_id'][0]]]], { 'fields': ['id']})
+
+  print(f'\033[0;32m[INFO]\033[0m\tEliminando relaciones ({len(teacher_of_subjects)}) entre {id[0]["name"]} y los módulos que impartía')
+  if len(teacher_of_subjects):
+    for ids in teacher_of_subjects:
+        models.execute_kw(db, uid, password, 'maya_core.subject_employee_rel', 'unlink', [[ids['id']]])
+
+for tsr in teachers_subjects_rel:  
+  print("\033[0;32m[INFO]\033[0m Procesando", tsr['teacher'])
   try:
    
     # hay que buscar si el empleado existe.
     # obtengo el id del usuario
     id = models.execute_kw(db, uid, password, 'res.users', 'search_read', 
-                                            [[['login','=', teacher]]], { 'fields': ['id', 'maya_employee_id']})
+                                            [[['login','=', tsr['teacher']]]], { 'fields': ['id', 'maya_employee_id', 'name']})
     
     if len(id) == 0:
-      raise Exception(f'No existe el usuario {teacher} en Odoo.')
+      raise Exception(f'No existe el usuario {tsr["teacher"]} en Odoo.')
     
     if id[0]['maya_employee_id'] == False:
-      raise Exception(f'No existe un empleado asociado al usuario {teacher}.')
+      raise Exception(f'No existe un empleado asociado al usuario {tsr["teacher"]}.')
     
     # se comprueba que la relación módulo/ciclo exista
-    for subj in teacher_subjects[teacher]:
-      if not any(course_id == courses_ec[subj[0]] for course_id in subjects_ec[subj[1]][1]):
-         raise Exception(f'El módulo {subj[2]} no se cursa en {subj[0]}. Profesor {teacher}')
-
-    # se eliminan, si las hay, las relaciones previas con otros módulos
-    teacher_of_subjects = models.execute_kw(db, uid, password, 
-                                            'maya_core.subject_employee_rel', 
-                                            'search_read', 
-                                            [[['employee_id','=', id[0]['maya_employee_id'][0]]]], { 'fields': ['id']})
-
-    if len(teacher_of_subjects):
-      for ids in teacher_of_subjects:
-        models.execute_kw(db, uid, password, 'maya_core.subject_employee_rel', 'unlink', [[ids['id']]])
+    if not any(course_id == courses_ec[tsr['course']][0] for course_id in subjects_ec[tsr['subject']][1]):
+      raise Exception(f'El módulo {tsr["subject"]} no se cursa en {tsr["course"]}. Profesor/a {id[0]["name"]} ({tsr["teacher"]})')
+  
 
     # enlazo los módulos que imparte
-    for subj in teacher_subjects[teacher]:
-      models.execute_kw(db, uid, password, 'maya_core.subject_employee_rel','create', [{
-        'course_id': courses_ec[subj[0]],
-        'subject_id': subjects_ec[subj[1]][0],
-        'employee_id': id[0]['maya_employee_id'][0]
-      }])
-      print("\033[0;32m[INFO]\033[0m\tAsociando ", teacher, "->", subj[2],"/", subj[0])
+    models.execute_kw(db, uid, password, 'maya_core.subject_employee_rel','create', [{
+            'course_id': courses_ec[tsr['course']][0],
+            'subject_id': subjects_ec[tsr['subject']][0],
+            'employee_id': id[0]['maya_employee_id'][0]
+          }])
 
+    line_count_OK += 1  
+    print(f'\033[0;32m[INFO]\033[0m\t   Asociando {id[0]["name"]} ({tsr["teacher"]} -> {subjects_ec[tsr["subject"]][2]} (id: {subjects_ec[tsr["subject"]][0]}) / {courses_ec[tsr["course"]][1]} (id: {courses_ec[tsr["course"]][0]})')
+    
   except (xmlrpc.client.Fault) as e:
     print('   \033[0;31m[ERROR]\033[0m ' + e.faultString)
     line_count_ERROR += 1
@@ -157,6 +171,6 @@ for teacher in teacher_subjects:
     print('   \033[0;31m[ERROR]\033[0m ' + str(e))
     line_count_ERROR += 1
 
-print(f'\033[0;32m[INFO]\033[0m Procesados {line_count_OK} usuarios / Errores: {line_count_ERROR}.')
+print(f'\033[0;32m[INFO]\033[0m Procesadas {line_count_OK} relaciones / Errores: {line_count_ERROR}.')
 
 
