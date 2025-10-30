6fdb8a03f83eba351128c796a2bff7526d43915c
Alfredo Oltra <aoltra@gmail.com>
2025-10-30 23:13:06 +0000

    feat: se añade el tipo de enseñanza

--- models/course.py
+++ models/course.py
@@ -1,15 +1,21 @@
-from odoo import models, fields
+from odoo import models, fields, _
 
 class Course(models.Model):
-    """
-    Define un ciclo formativo
-    """
+  """  
+  Define un curso: ciclo formativo, tipo bachillerato, etc
+  """
 
-    _name = 'maya_core.course'
-    _description = 'Ciclo Formativo'
+  _name = 'maya_core.course'
+  _description = 'Ciclo Formativo'
 
-    abbr = fields.Char('Abreviatura', size = 5,required = True, translate=True)
-    name = fields.Char('Ciclo', required = True, translate=True)
-    code = fields.Char('Código', required = True, size = 6)
+  abbr = fields.Char('Abreviatura', size = 5,required = True, translate=True)
+  name = fields.Char('Ciclo', required = True, translate=True)
+  code = fields.Char('Código', required = True, size = 6)
 
-    subjects_ids = fields.Many2many('maya_core.subject', string = 'Módulos')
+  teaching = fields.Selection([
+    ('4', _('Bachillerato')),
+    ('5', _('Ciclos')),
+    ('7', _('ESPA'))
+     ], string = _('Tipo de enseñanza'), default = '7')
+
+  subjects_ids = fields.Many2many('maya_core.subject', string = 'Módulos')
